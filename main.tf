56a1cb96ebbbee24eb8e1efd995c070b5c5ea891
jogaleins <adpazon@gmail.com>
2022-09-17 12:14:22 +0800

    modified main.tf

--- main.tf
+++ main.tf
@@ -17,50 +17,39 @@ provider "proxmox" {
 }
 
 resource "proxmox_vm_qemu" "cloudinit-test" {
-    name = "terraform-test-vm"
-    desc = "A test for using terraform and cloudinit"
-
-    # Node name has to be the same name as within the cluster
-    # this might not include the FQDN
+    count = 2
+    name = "tf-vm-${count.index + 1}"
     target_node = "homelab"
-
-    # The destination resource pool for the new VM
-
-    # The template name to clone this vm from
+    vmid = "40${count.index + 1}"
     clone = "centos"
 
-    # Activate QEMU agent for this VM
     agent = 1
 
     os_type = "cloud-init"
-    cores = 2
+    cores = 4
     sockets = 1
     vcpus = 0
     cpu = "host"
     memory = 2048
-    scsihw = "lsi"
+    scsihw = "virtio-scsi-pci"
 
-    # Setup the disk
     disk {
         size = "10G"
-        type = "virtio"
+        type = "scsi"
         storage = "pve-ssd"
         iothread = 1
         ssd = 1
         discard = "on"
     }
-    # Setup the network interface and assign a vlan tag: 256
     network {
         model = "virtio"
         bridge = "vmbr0"
-        tag = 256
     }
-
-    # Setup the ip address using cloud-init.
-    # Keep in mind to use the CIDR notation for the ip.
-    ipconfig0 = "ip=192.168.10.20/24,gw=192.168.10.1"
-
-    sshkeys = <<EOF
-    ssh-rsa 9182739187293817293817293871== user@pc
-    EOF
+  
+    lifecycle {
+      ignore_changes = [
+      network,
+      ]
+    }
+    ipconfig0 = "ip=192.168.2.15${count.index + 1}/24,gw=192.168.2.1"
 }
